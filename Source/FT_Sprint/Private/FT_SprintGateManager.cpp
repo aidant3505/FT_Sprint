fe872b450658ea00d3a5e6cdf9669fc867daad6f
connahKendrickMMU <44666644+connahKendrickMMU@users.noreply.github.com>
2025-11-29 18:04:48 +0800

    bug fixed
    I didn't initialize the variable properly in the constructor like a idiot.
    
    Main issue is the connection for the singleton is bad as Unreal backend messes with it

--- Source/FT_Sprint/Private/FT_SprintGateManager.cpp
+++ Source/FT_Sprint/Private/FT_SprintGateManager.cpp
@@ -3,7 +3,7 @@
 
 #include "FT_SprintGateManager.h"
 
-// Define the static instance here which is needed in C++ files <- Stupid C++
+// Define the static instance here which is needed in C++ files <- Stupid C++, and unreal build system! we should use a UGameInstanceSubsystem for this!
 AFT_SprintGateManager* AFT_SprintGateManager::instance;
 
 // Sets default values
@@ -11,12 +11,15 @@ AFT_SprintGateManager::AFT_SprintGateManager()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	gatesPassed = 0;
+	timePassed = 0;
 }
 
 // Called when the game starts or when spawned
 void AFT_SprintGateManager::BeginPlay()
 {
 	Super::BeginPlay();
+
 	// check for multiple managers in level
 	if(instance == nullptr)
 	{
@@ -59,16 +62,19 @@ void AFT_SprintGateManager::Tick(float DeltaTime)
 			ResetTrack();
 		}
 	}
-
 }
 
 // method to tell all the sprint gates the track is complete shutdown
 void AFT_SprintGateManager::CompleteTrack()
 {
 	UE_LOG(LogTemp, Warning, TEXT("Track complete! Smashed it!"));
+	gatesPassed = 0;
+	timePassed = 0.0f;
 	for (AFT_SprintGate* gate : sprintGates)
-	{
-		gate->completedTrack = true;
+	{	if(gate)
+		{ 
+			gate->completedTrack = true;
+		}
 	}
 }
 
@@ -78,13 +84,16 @@ void AFT_SprintGateManager::GatePassed()
 	UE_LOG(LogTemp, Warning, TEXT("Gate Passed!"));
 	// keep track of the gates passed
 	gatesPassed++;
+	// reset the clock, you could probably improve this a bit
+	timePassed = 0;
+	UE_LOG(LogTemp, Warning, TEXT("Total Gates Passed: %d"), gatesPassed);
+	UE_LOG(LogTemp, Warning, TEXT("Total Sprint Gates found: %d"), sprintGates.Num());
 	// if all gates are passed then complete the track
 	if (gatesPassed >= sprintGates.Num())
 	{
 		CompleteTrack();
 	}
-	UE_LOG(LogTemp, Warning, TEXT("Total Gates Passed: %d"), gatesPassed);
-	UE_LOG(LogTemp, Warning, TEXT("Total Sprint Gates found: %d"), sprintGates.Num());
+	
 }
 
 // reset all the gates to false, and reset the tracking variables
@@ -101,3 +110,12 @@ void AFT_SprintGateManager::ResetTrack()
 	timePassed = 0.0f;
 }
 
+void AFT_SprintGateManager::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+	UE_LOG(LogTemp, Warning, TEXT("Sprint Gate Manager EndPlay called."));
+	if (instance == this)
+	{
+		instance = nullptr;
+	}
+}
